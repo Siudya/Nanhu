88b56bd70a17f3ad9c9ed114c090346274a34513
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-06-16 14:58:22 +0800

    Fix lpv shift logics

--- src/main/scala/xiangshan/backend/issue/FpRs/FloatingStatusArray.scala
+++ src/main/scala/xiangshan/backend/issue/FpRs/FloatingStatusArray.scala
@@ -53,6 +53,7 @@ class FloatingIssueInfoGenerator(implicit p: Parameters) extends XSModule{
   io.out.bits.pdest := ib.pdest
   io.out.bits.fpWen := ib.fpWen
   io.out.bits.rfWen := ib.rfWen
+  io.out.bits.lpv.zip(ib.lpv.transpose).foreach({case(o, i) => o := i.reduce(_|_)})
 }
 class FloatingStatusArrayEntry(implicit p: Parameters) extends BasicStatusArrayEntry(3){
   val state = EntryState()
