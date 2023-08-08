1ce3e6cd5bddc240b44ddd8156af3110e9094543
lmf-bit <68804856+lmf-bit@users.noreply.github.com>
2023-08-08 10:55:26 +0800

    memblock merge
    Co-authored-by: Manfred <none>

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -63,6 +63,7 @@ class MinimalConfig(n: Int = 1) extends Config(
         DecodeWidth = 2,
         RenameWidth = 2,
         FetchWidth = 4,
+        IssQueSize = 8,
         NRPhyRegs = 64,
         LoadQueueSize = 16,
         LoadQueueNWriteBanks = 4,
