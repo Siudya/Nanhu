a8a793eb3077e1006849422c9a111acc0843e14b
zhang dongrong <zhangdongrong@open03.boscszcluster.com>
2024-03-05 09:46:49 +0800

    madd bootrom_disable GPIO & modify Makefile to support bootrom disable

--- src/main/scala/device/ROT_rstmgr.scala
+++ src/main/scala/device/ROT_rstmgr.scala
@@ -24,13 +24,26 @@ class ROT_rstmgr(implicit p: Parameters) extends LazyModule {
   // lazy val module = new LazyModuleImp(this) {
     val io = IO(new Bundle {
       val ctrl = Output(Bool())
+      val key = Output(UInt(256.W))
+      val enable = Output(Bool())
     })
 
     val reset_value = RegInit(0.U(32.W))
+    val key_value = RegInit(0.U(256.W))
+    val enable_value = RegInit(false.B)
+
     io.ctrl := reset_value(0)
+    io.key := key_value
+    io.enable := enable_value
 
     node.regmap(
       0x00 -> Seq(RegField(32, reset_value, RegFieldDesc("ctrl", "rst ctrl Register"))),
+      // key register spans four 64-bit addresses
+      0x08 -> Seq(RegField(64, key_value(63, 0), RegFieldDesc("key_part1", "Key Register Part 1"))),
+      0x10 -> Seq(RegField(64, key_value(127, 64), RegFieldDesc("key_part2", "Key Register Part 2"))),
+      0x18 -> Seq(RegField(64, key_value(191, 128), RegFieldDesc("key_part3", "Key Register Part 3"))),
+      0x20 -> Seq(RegField(64, key_value(255, 192), RegFieldDesc("key_part4", "Key Register Part 4"))),
+      0x28 -> Seq(RegField(1, enable_value, RegFieldDesc("enable", "Enable Register")))
     )
  
   }
